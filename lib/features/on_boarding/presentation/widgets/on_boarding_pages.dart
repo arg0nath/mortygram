c893c9980a3da08fd4c05e82c01a84b0dfa362e6
arg0nath <32592902+arg0nath@users.noreply.github.com>
2026-02-15 16:24:48 +0200

    feat: Add AppPalette class for color gradients and update on-boarding page descriptions

--- lib/features/on_boarding/presentation/widgets/on_boarding_pages.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_pages.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mortygram/config/theme/app_palette.dart';
+import 'package:mortygram/config/theme/presentation/app_palette.dart';
 import 'package:mortygram/features/on_boarding/domain/entities/on_boarding_data.dart';
 
 const List<OnBoardingData> onBoardingPages = <OnBoardingData>[
@@ -7,7 +7,7 @@ const List<OnBoardingData> onBoardingPages = <OnBoardingData>[
   OnBoardingData(title: 'Discover\n Characters', description: 'Search characters, view details of them', icon: Icons.search_rounded, gradient: AppPalette.purpleGradient),
   OnBoardingData(
     title: 'Share &\nConnect',
-    description: 'Share your favorite character and insights with friends. Join the crypto community today!',
+    description: 'Share your favorite character and insights with friends!',
     icon: Icons.share_rounded,
     gradient: AppPalette.greenGradient,
   ),
