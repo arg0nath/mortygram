056fcc7374030517f6d158ad33b72ba82db2394e
arg0nath <32592902+arg0nath@users.noreply.github.com>
2026-02-17 21:52:33 +0200

    refactor: Enhance CustomLoadingIndicator with animation and loading text

--- lib/core/common/widgets/custom_loading_indicator.dart
+++ lib/core/common/widgets/custom_loading_indicator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lottie/lottie.dart';
+import 'package:mortygram/core/common/extensions/context_ext.dart';
 import 'package:mortygram/core/common/res/app_assets.dart';
 
 class CustomLoadingIndicator extends StatelessWidget {
@@ -7,11 +9,17 @@ class CustomLoadingIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LottieBuilder.asset(
-      AppAssets.loadingAnimation,
-      repeat: true,
-      height: 200,
-      width: 200,
-    );
+    return SizedBox(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 16,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Loading...', style: context.textTheme.titleLarge),
+          LottieBuilder.asset(AppAssets.loadingAnimation, repeat: true, height: 200, width: 200),
+        ],
+      ),
+    ).animate().fadeIn(delay: 100.ms);
   }
 }
